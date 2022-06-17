c81a0ed74b77768c966df785ea0cb5384b9fe9ab
ModyQyW <wurui-dev@foxmail.com>
2022-06-17 18:55:19 +0800

    fix: 移除多余设置

--- uni-app/src/helpers/action-sheet.ts
+++ uni-app/src/helpers/action-sheet.ts
@@ -1,5 +1,4 @@
 export const showActionSheet = (options: UniApp.ShowActionSheetOptions) =>
   uni.showActionSheet({
-    itemColor: '#303133',
     ...options,
   });
--- uni-app/src/helpers/modal.ts
+++ uni-app/src/helpers/modal.ts
@@ -1,7 +1,5 @@
 export const showModal = (options: UniApp.ShowModalOptions) =>
   uni.showModal({
     title: '提示',
-    cancelColor: '#303133',
-    confirmColor: '#409eff',
     ...options,
   });
