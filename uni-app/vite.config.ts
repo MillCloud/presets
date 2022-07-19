b09b1883dcb0c4c6b3e547996c9c07f0f1118f83
ModyQyW <wurui-dev@foxmail.com>
2022-07-19 16:55:23 +0800

    chore: update vite config

--- uni-app/vite.config.ts
+++ uni-app/vite.config.ts
@@ -1,10 +1,14 @@
-import path from 'node:path';
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+import vueDefineOptions from 'unplugin-vue-define-options/vite';
 import tailwindcss from 'tailwindcss';
 // @ts-ignore
 import postcssPresetEnv from 'postcss-preset-env';
+import autoImport from 'unplugin-auto-import/vite';
+import vueComponents from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import icons from 'unplugin-icons/vite';
 import env from 'vite-plugin-env-compatible';
 import eslint from '@modyqyw/vite-plugin-eslint';
 import stylelint from 'vite-plugin-stylelint';
@@ -46,21 +50,46 @@ export default defineConfig({
         targets: ['ios >= 10', 'chrome >= 53'],
       },
     }),
+    vueDefineOptions(),
+    autoImport({
+      dirs: ['src/composables', 'src/stores'],
+      imports: ['vue', 'vue/macros', 'pinia', '@vueuse/core'],
+    }),
+    vueComponents({
+      dirs: ['src/components'],
+      resolvers: [
+        {
+          type: 'component',
+          resolve: (componentName) => {
+            if (componentName === 'VIcon') {
+              return { name: 'Icon', from: '@iconify/vue' };
+            }
+          },
+        },
+        IconsResolver(),
+      ],
+      types: [],
+    }),
+    icons({
+      compiler: 'vue3',
+      defaultClass: 'el-icon',
+    }),
     env({
       prefix: 'VITE',
     }),
     eslint({
       fix: true,
+      lintOnStart: true,
     }),
     stylelint({
       fix: true,
+      lintOnStart: true,
     }),
     inspect(),
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@/': `${path.resolve('src')}/`,
+      '@': fileURLToPath(new URL('src', import.meta.url)),
     },
   },
 });
