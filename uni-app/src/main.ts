98fe8acad0f4dae22c5a9ed24a5c8b678939728c
ModyQyW <wurui-dev@foxmail.com>
2022-07-19 16:55:12 +0800

    chore: update import

--- uni-app/src/main.ts
+++ uni-app/src/main.ts
@@ -1,11 +1,14 @@
+import { createSSRApp } from 'vue';
+
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { createSSRApp } from 'vue';
+
 import { VueQueryPlugin } from 'vue-query';
+
 import App from './App.vue';
-import { pinia } from './stores';
 import { vueQueryPluginOptions } from './helpers';
+
 import '@modyqyw/tailwind-presets/miniprogram-base.css';
 import '@/styles/tailwind.css';
 import '@/styles/global.scss';
