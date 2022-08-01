dd8930f751fb3ab6dd6bf302acc9bcb00edd765e
ModyQyW <wurui-dev@foxmail.com>
2022-08-01 21:38:42 +0800

    chore: update types

--- uni-app/src/global.d.ts
+++ uni-app/src/global.d.ts
@@ -1,52 +1,91 @@
+import type {
+  UrError,
+  UrBaseResponse,
+  UrRequestResponse,
+  UrDownloadResponse,
+  UrUploadResponse,
+  UrBaseConfig,
+  UrRequestConfig,
+  UrDownloadConfig,
+  UrUploadConfig,
+} from '@modyqyw/uni-request';
+
 declare global {
-  interface IResponseData {
+  /* common */
+  type TShowErrorType = 'toast' | 'modal';
+
+  type TRequestData = Record<string, any>;
+
+  type TResponseData = {
     success: boolean;
     code: string;
     message: string;
-    [propName: string]: any;
-  }
-
-  type BaseRequestHeaders = Record<string, string | number | boolean>;
-
-  type BaseResponseHeaders = Record<string, string> & {
-    'set-cookie'?: string[];
+    [key: string]: any;
   };
 
-  type BaseData = string | AnyObject | ArrayBuffer;
-
-  interface BaseRequestConfig<D = BaseData>
-    extends Omit<UniApp.RequestOptions, 'success' | 'fail' | 'complete'> {
-    url?: string;
-    data?: D;
-    params?: string | AnyObject;
-    headers?: BaseRequestHeaders;
-  }
-
-  type BaseResponse<T = BaseData, D = BaseData> = UniApp.RequestSuccessCallbackResult & {
-    data: T;
-    status?: number;
-    statusText?: string;
-    headers?: BaseResponseHeaders;
-    config?: BaseRequestConfig<D>;
-    request?: any;
-  };
-
-  type BaseError<T = BaseData, D = BaseData> = UniApp.GeneralCallbackResult & {
-    config?: BaseRequestConfig<D>;
-    code?: string;
-    request?: any;
-    response?: BaseResponse<T, D>;
-  };
-
-  interface IResponse extends BaseResponse<IResponseData> {}
-
-  interface IResponseError extends BaseError<IResponseData> {
-    response?: IResponse;
-  }
-
-  interface IRequestConfig extends BaseRequestConfig {
+  /* config */
+  interface IBaseConfig<T = TResponseData, D = TRequestData> extends UrBaseConfig<T, D> {
     showError?: boolean;
-    showErrorType?: 'toast' | 'modal';
+    showErrorType?: TShowErrorType;
+  }
+
+  interface IRequestConfig<T = TResponseData, D = TRequestData>
+    extends UrRequestConfig<T, D>,
+      IBaseConfig<T, D> {}
+
+  interface IDownloadConfig<T = TResponseData, D = TRequestData>
+    extends UrDownloadConfig<T, D>,
+      IBaseConfig<T, D> {}
+
+  interface IUploadConfig<T = TResponseData, D = TRequestData>
+    extends UrUploadConfig<T, D>,
+      IBaseConfig<T, D> {}
+
+  type TConfig =
+    | IBaseConfig<T, D>
+    | IRequestConfig<T, D>
+    | IDownloadConfig<T, D>
+    | IUploadConfig<T, D>;
+
+  /* response */
+  interface IBaseResponse<T = TResponseData, D = TRequestData> extends UrBaseResponse<T, D> {}
+
+  interface IRequestResponse<T = TResponseData, D = TRequestData>
+    extends UrRequestResponse<T, D>,
+      IBaseResponse<T, D> {}
+
+  interface IDownloadResponse<T = TResponseData, D = TRequestData>
+    extends UrDownloadResponse<T, D>,
+      IBaseResponse<T, D> {}
+
+  interface IUploadResponse<T = TResponseData, D = TRequestData>
+    extends UrUploadResponse<T, D>,
+      IBaseResponse<T, D> {}
+
+  type TResponse =
+    | IBaseResponse<T, D>
+    | IRequestResponse<T, D>
+    | IDownloadResponse<T, D>
+    | IUploadResponse<T, D>;
+
+  /* promise */
+  type TBasePromise<T = TResponseData, D = TRequestData> = Promise<IBaseResponse<T, D>>;
+
+  type TRequestPromise<T = TResponseData, D = TRequestData> = Promise<IRequestResponse<T, D>>;
+
+  type TDownloadPromise<T = TResponseData, D = TRequestData> = Promise<IDownloadResponse<T, D>>;
+
+  type TUploadPromise<T = TResponseData, D = TRequestData> = Promise<IUploadResponse<T, D>>;
+
+  type TPromise<T = TResponseData, D = TRequestData> =
+    | TBasePromise<T, D>
+    | TRequestPromise<T, D>
+    | TDownloadPromise<T, D>
+    | TUploadPromise<T, D>;
+
+  /* error */
+  interface IError<T = TResponseData, D = TRequestData> extends UrError<T, D> {
+    response?: IBaseResponse<T, D>;
   }
 }
 
