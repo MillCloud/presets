c697ee0a1d617deef956a6da209f489d2d59927d
ModyQyW <wurui-dev@foxmail.com>
2022-08-07 19:46:47 +0800

    fix: fix tailwind config

--- uni-app/tailwind.config.cjs
+++ uni-app/tailwind.config.cjs
@@ -1,4 +1,5 @@
 const basePreset = require('@modyqyw/tailwind-presets/base').default;
+const getElementPlusPreset = require('@modyqyw/tailwind-presets/element-plus').default;
 const miniprogramPreset = require('@modyqyw/tailwind-presets/miniprogram').default;
 const tailwindcssTypography = require('@tailwindcss/typography');
 const tailwindcssLineClamp = require('@tailwindcss/line-clamp');
@@ -6,6 +7,6 @@ const tailwindcssAspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  presets: [basePreset, miniprogramPreset],
+  presets: [basePreset, getElementPlusPreset(), miniprogramPreset],
   plugins: [tailwindcssTypography, tailwindcssLineClamp, tailwindcssAspectRatio],
 };
