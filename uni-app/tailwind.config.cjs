eb8f40d1b9ae4deaf624f5bf2e9817cb4760b764
ModyQyW <wurui-dev@foxmail.com>
2022-08-20 20:22:16 +0800

    chore: update tailwind config

--- uni-app/tailwind.config.cjs
+++ uni-app/tailwind.config.cjs
@@ -5,8 +5,12 @@ const tailwindcssTypography = require('@tailwindcss/typography');
 const tailwindcssLineClamp = require('@tailwindcss/line-clamp');
 const tailwindcssAspectRatio = require('@tailwindcss/aspect-ratio');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   presets: [basePreset, getElementPlusPreset(), miniprogramPreset],
+  theme: {
+    screens: {},
+  },
   plugins: [tailwindcssTypography, tailwindcssLineClamp, tailwindcssAspectRatio],
 };
