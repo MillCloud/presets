9b7c4f0faaf9b835ec78c7efc557322203ea5676
ModyQyW <wurui-dev@foxmail.com>
2022-08-23 11:53:52 +0800

    chore: update tailwind config

--- vue/tailwind.config.cjs
+++ vue/tailwind.config.cjs
@@ -1,11 +1,10 @@
-const basePreset = require('@modyqyw/tailwind-presets/base').default;
-const getElementPlusPreset = require('@modyqyw/tailwind-presets/element-plus').default;
+const { base, elementPlus } = require('@modyqyw/tailwind-presets');
 const tailwindcssTypography = require('@tailwindcss/typography');
 const tailwindcssLineClamp = require('@tailwindcss/line-clamp');
 const tailwindcssAspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  presets: [basePreset, getElementPlusPreset()],
+  presets: [base, elementPlus()],
   plugins: [tailwindcssTypography, tailwindcssLineClamp, tailwindcssAspectRatio],
 };
