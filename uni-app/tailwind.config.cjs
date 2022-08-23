0d8d1ed94bcccc0ca8c30bbe570b3249e4034d10
ModyQyW <wurui-dev@foxmail.com>
2022-08-23 11:56:52 +0800

    chore: update tailwind config

--- uni-app/tailwind.config.cjs
+++ uni-app/tailwind.config.cjs
@@ -1,16 +1,16 @@
-const basePreset = require('@modyqyw/tailwind-presets/base').default;
-const getElementPlusPreset = require('@modyqyw/tailwind-presets/element-plus').default;
-const miniprogramPreset = require('@modyqyw/tailwind-presets/miniprogram').default;
-const tailwindcssTypography = require('@tailwindcss/typography');
-const tailwindcssLineClamp = require('@tailwindcss/line-clamp');
-const tailwindcssAspectRatio = require('@tailwindcss/aspect-ratio');
+const {
+  base,
+  elementPlus,
+  miniprogramBase,
+  miniprogramScreens,
+} = require('@modyqyw/tailwind-presets');
+const typography = require('@tailwindcss/typography');
+const lineClamp = require('@tailwindcss/line-clamp');
+const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  presets: [basePreset, getElementPlusPreset(), miniprogramPreset],
-  theme: {
-    screens: {},
-  },
-  plugins: [tailwindcssTypography, tailwindcssLineClamp, tailwindcssAspectRatio],
+  presets: [base, elementPlus(), miniprogramBase, miniprogramScreens],
+  plugins: [typography, lineClamp, aspectRatio],
 };
--- vue/tailwind.config.cjs
+++ vue/tailwind.config.cjs
@@ -1,10 +1,10 @@
 const { base, elementPlus } = require('@modyqyw/tailwind-presets');
-const tailwindcssTypography = require('@tailwindcss/typography');
-const tailwindcssLineClamp = require('@tailwindcss/line-clamp');
-const tailwindcssAspectRatio = require('@tailwindcss/aspect-ratio');
+const typography = require('@tailwindcss/typography');
+const lineClamp = require('@tailwindcss/line-clamp');
+const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   presets: [base, elementPlus()],
-  plugins: [tailwindcssTypography, tailwindcssLineClamp, tailwindcssAspectRatio],
+  plugins: [typography, lineClamp, aspectRatio],
 };
