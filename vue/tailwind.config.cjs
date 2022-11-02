b66fdd372213bc4be61428fde63af3ab28d602ba
ModyQyW <wurui-dev@foxmail.com>
2022-11-02 10:36:50 +0800

    chore: update tailwind config

--- vue/tailwind.config.cjs
+++ vue/tailwind.config.cjs
@@ -1,10 +1,9 @@
 const { basePreset, elementPlusPreset } = require('tailwind-extensions');
 const typography = require('@tailwindcss/typography');
 const lineClamp = require('@tailwindcss/line-clamp');
-const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   presets: [basePreset, elementPlusPreset()],
-  plugins: [typography, lineClamp, aspectRatio],
+  plugins: [typography, lineClamp],
 };
