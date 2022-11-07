f3560c428f59f0cc3960587450cf38d5c4bf49e9
ModyQyW <wurui-dev@foxmail.com>
2022-11-07 17:26:39 +0800

    feat: translate time by default

--- koa/src/helpers/index.ts
+++ koa/src/helpers/index.ts
@@ -4,6 +4,7 @@ import pinoPretty from 'pino-pretty';
 export const pinoPrettyStream = pinoPretty({
   colorize: true,
   levelFirst: true,
+  translateTime: true,
 });
 
 export const logger = pino(pinoPrettyStream);
