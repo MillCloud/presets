2b3cbfac1cc00812a9668133037a32f0fe750173
ModyQyW <wurui-dev@foxmail.com>
2022-11-18 21:58:54 +0800

    chore: update translateTime

--- koa/src/helpers/index.ts
+++ koa/src/helpers/index.ts
@@ -4,7 +4,7 @@ import pinoPretty from 'pino-pretty';
 export const pinoPrettyStream = pinoPretty({
   colorize: true,
   levelFirst: true,
-  translateTime: true,
+  translateTime: 'SYS:standard',
 });
 
 export const logger = pino(pinoPrettyStream);
