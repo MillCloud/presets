c78d9518f2a2f993de5bcb33f1368dfc91a54fc0
ModyQyW <wurui-dev@foxmail.com>
2023-01-06 14:13:31 +0800

    chore: update config

--- uni-app/.lintstagedrc.cjs
+++ uni-app/.lintstagedrc.cjs
@@ -1,5 +1,7 @@
 module.exports = {
   '*.md': 'markdownlint --fix',
-  '*.{css,less,scss,sass,vue}': 'stylelint --fix',
-  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix',
+  '*.{css,less,scss,sass,vue}':
+    'stylelint --fix --cache --allow-empty-input --ignore-path=.gitignore',
+  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}':
+    'eslint --fix --cache --no-error-on-unmatched-pattern --ignore-path=.gitignore',
 };
