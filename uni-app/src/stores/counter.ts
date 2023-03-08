4b6a152c82d04c6e612a75c5bbeb83f2fc8ea171
ModyQyW <wurui-dev@foxmail.com>
2023-03-08 09:24:17 +0800

    chore: store example

--- uni-app/src/stores/counter.ts
+++ uni-app/src/stores/counter.ts
@@ -1,14 +1,10 @@
-export const useCounterStore = defineStore({
-  id: 'counter',
-  state: () => ({
-    counter: 0,
-  }),
-  getters: {
-    doubleCounter: (state) => state.counter * 2,
-  },
-  actions: {
-    increase() {
-      this.counter += 1;
-    },
-  },
+import { ref, computed } from 'vue';
+
+export const useCounterStore = defineStore('counter', () => {
+  const counter = ref(0);
+  const doubleCounter = computed(() => counter.value * 2);
+  const increase = () => {
+    counter.value += 1;
+  };
+  return { counter, doubleCounter, increase };
 });
