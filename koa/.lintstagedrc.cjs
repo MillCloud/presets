9f38898c257fde973bb7f420a6182b5eaf5a18e4
ModyQyW <wurui-dev@foxmail.com>
2023-04-26 16:53:19 +0800

    chore: update

--- koa/.lintstagedrc.cjs
+++ koa/.lintstagedrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   'package.json': 'sort-package-json',
-  '*.md': 'markdownlint --fix --ignore=CHANGELOG.md --ignore-path=.gitignore',
+  '*.md': 'markdownlint --fix --ignore-path=.gitignore',
   '*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache --ignore-path=.gitignore',
+  '*.{ts,cts,mts,tsx}': () => 'tsc -p tsconfig.json --noEmit'
 };
