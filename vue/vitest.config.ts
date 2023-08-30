ef6cd92a9fd620d868047a7dfb6bb09618cb9e6b
ModyQyW <wurui-dev@foxmail.com>
2023-08-30 21:56:11 +0800

    test(vue): [vitest] update

--- vue/vitest.config.ts
+++ vue/vitest.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default defineConfig({
-  ...viteConfig,
-});
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+);
