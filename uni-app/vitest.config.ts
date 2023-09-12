00d8e434cb59b5f32b596a63542ceef92370a45e
ModyQyW <wurui-dev@foxmail.com>
2023-09-12 20:24:33 +0800

    chore(uni-app): update vitest config

--- uni-app/vitest.config.ts
+++ uni-app/vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default defineConfig({
-  ...viteConfig,
-  test: {
-    singleThread: true,
-  },
-});
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      singleThread: true,
+    },
+  }),
+);
