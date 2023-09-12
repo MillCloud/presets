db912c1edb37e6c338a46aacd3bf85eca423e646
ModyQyW <wurui-dev@foxmail.com>
2023-09-12 20:16:19 +0800

    chore(uni-app): update lint-staged config

--- uni-app/.lintstagedrc.cjs
+++ uni-app/.lintstagedrc.cjs
@@ -3,5 +3,5 @@ module.exports = {
   '*.md': 'markdownlint --fix --ignore-path=.gitignore',
   '*.{css,scss,vue}': 'stylelint --fix --cache --ignore-path=.gitignore',
   '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix --cache --ignore-path=.gitignore',
-  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc -p tsconfig.json --noEmit'
+  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc --noEmit -p tsconfig.vitest.json --composite false',
 };
