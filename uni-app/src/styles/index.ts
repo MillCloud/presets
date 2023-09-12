e05fafc33a0009eb3bb1a463592280f4c617d5dc
ModyQyW <wurui-dev@foxmail.com>
2023-09-12 20:28:17 +0800

    feat(uni-app): 调整样式处理

---
+++ uni-app/src/styles/index.ts
@@ -0,0 +1,7 @@
+import './button-preflight.css';
+import 'modern-normalize';
+import '@unocss/reset/tailwind-compat.css';
+import './preflight.css';
+import './nutui.scss';
+import './global.scss';
+import 'uno.css';
