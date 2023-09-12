d9d733650140fb7bffce712b0701e7ef677be740
ModyQyW <wurui-dev@foxmail.com>
2023-09-12 20:24:51 +0800

    chore(uni-app): update pages config

--- uni-app/pages.config.ts
+++ uni-app/pages.config.ts
@@ -1,12 +1,6 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages';
 
 export default defineUniPages({
-  easycom: {
-    autoscan: true,
-    custom: {
-      '^uni-(.*)': '@dcloudio/uni-ui/lib/uni-$1/uni-$1.vue',
-    },
-  },
   globalStyle: {
     navigationBarTextStyle: 'black',
     navigationBarTitleText: 'uni-app',
