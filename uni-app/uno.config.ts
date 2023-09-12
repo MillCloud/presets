47984a0236fadff76733b2cdf08e2d229b71fc57
ModyQyW <wurui-dev@foxmail.com>
2023-09-12 20:19:50 +0800

    chore(uni-app): update unocss config

--- uni-app/uno.config.ts
+++ uni-app/uno.config.ts
@@ -10,7 +10,7 @@ import {
 } from 'unocss';
 import presetRemToPx from '@unocss/preset-rem-to-px';
 // import { presetAntd } from 'unocss-preset-antd';
-import { presetElementPlus } from 'unocss-preset-element-plus';
+// import { presetElementPlus } from 'unocss-preset-element-plus';
 // import { presetNaiveUi } from 'unocss-preset-naive-ui';
 import { transformerAttributify } from 'unocss-applet';
 
@@ -23,13 +23,13 @@ const presets = [
   presetTypography(),
   presetWebFonts(),
   // presetAntd(),
-  presetElementPlus({
-    preferCssVariables: false,
-    selectors: {
-      light: ':root,page,.root,page',
-      dark: '.dark,.root.dark',
-    },
-  }),
+  // presetElementPlus({
+  //   preferCssVariables: false,
+  //   selectors: {
+  //     light: ':root,page,.root,page',
+  //     dark: '.dark,.root.dark',
+  //   },
+  // }),
   // presetNaiveUi({
   //   preferCssVariables: false,
   //   selectors: {
