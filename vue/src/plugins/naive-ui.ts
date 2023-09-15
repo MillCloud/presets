cc642867b443eec48bfb50e7a40356800ab49d6c
ModyQyW <wurui-dev@foxmail.com>
2023-09-15 10:03:47 +0800

    feat(vue): add naive-ui-pro-components

--- vue/src/plugins/naive-ui.ts
+++ vue/src/plugins/naive-ui.ts
@@ -1,8 +1,10 @@
 import naiveUi from 'naive-ui';
+import naiveUIProComponents from 'naive-ui-pro-components';
 import type { Plugin } from 'vue';
 
 export const naiveUiPlugin: Plugin = {
   install: (app) => {
     app.use(naiveUi);
+    app.use(naiveUIProComponents);
   },
 };
